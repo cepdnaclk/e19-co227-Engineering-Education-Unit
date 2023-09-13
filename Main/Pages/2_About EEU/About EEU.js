b5945961903274246bbf79fda7588d7f2d5ea613
Nithusikan <e19266@eng.pdn.ac.lk>
2023-09-13 19:34:38 +0530

    About EEU updated

--- Main/Pages/2_About EEU/About EEU.js
+++ Main/Pages/2_About EEU/About EEU.js
@@ -1,9 +1,9 @@
-import './App.css';
+import './About EEU.css';
 
-function App() {
+function AboutEEU() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="About-EEU">
+      <header className="About-EEU-header">
         <div className="imge"><img alt="Uop" src="../uop.jpg"/></div>
        
     <p>The Engineering Education Unit was established in the Faculty of Engineering in 1985 primarily with the view to coordinate activities in the following areas: </p>
@@ -13,7 +13,7 @@ function App() {
     <li>Provision of audio-visual facilities for educational purposes</li>
     <li>Freshmen orientation</li>
     </ul>
-    <p> In 2015 the Faculty Board assigned the responsibility of doing the overall coordination of all General Elective subjects. The coordination of subjects are done by different Departments.The responsibility of EEU includes, facilitating the appointment of visiting lectures, liaison with the visiting lecturers and the subject coordinators of the Departments, checking and recommending the payments for lecturers.</p>
+    <p> In 2015 the Faculty Board assigned the responsibility of doing the overall coordination of all General Elective subjects. The coordination of subjects are done by different Departments.The responsibility of EEU includes, facilitating the Appointment of visiting lectures, liaison with the visiting lecturers and the subject coordinators of the Departments, checking and recommending the payments for lecturers.</p>
     <p> The management and development of the EEU is the responsibility of a committee consisting of the Dean, the EEU Director and members of the permanent academic staff elected by the Faculty Board.</p>
     <p> The EEU has a professional audio-visual studio equipped with a advanced non-liner video production system. Members of the teaching staff of the Faculty have utilized these facilities to produce educational videos for use in their teaching.</p>
     <p>Three seminar rooms and conference room equipped with large screen multimedia projection facility, digital white boards, overhead projectors, are also maintained by the EEU.</p>
@@ -23,55 +23,5 @@ function App() {
   );
 }
 
-export default App;
+export default AboutEEU;
 
-
-.App {
-  text-align: center;
-}
-
-.App-logo {
-  height: 40vmin;
-  pointer-events: none;
-}
-
-@media (prefers-reduced-motion: no-preference) {
-  .App-logo {
-    animation: App-logo-spin infinite 20s linear;
-  }
-}
-
-.App-header {
-  background-color: #f1f5fd;
-  min-height: 100vh;
-  display: block;
-  flex-direction: column;
-  font-size: calc(10px + 2vmin);
-  color: black;
-}
-.imge {
-  height: 12rem;
-  width: 50rem;
-  border: 8px solid rgba(255,255,255,0.12);
-}
-
-.App-link {
-  color: #61dafb;
-}
-
-@keyframes App-logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-
-
-
-*{
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
