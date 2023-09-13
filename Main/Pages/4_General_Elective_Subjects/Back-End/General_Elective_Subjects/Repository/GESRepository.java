bccfce4fd6f3dfb51430253328231429283c53bf
Danujan <sivasundaralingamdanujan@gmail.com>
2023-09-13 18:35:11 +0530

    GES JPA Repository extension repository

---
+++ Main/Pages/4_General_Elective_Subjects/Back-End/General_Elective_Subjects/Repository/GESRepository.java
@@ -0,0 +1,8 @@
+package com.GES.General_Elective_Subjects.Repository;
+
+import com.GES.General_Elective_Subjects.Model.GES;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GESRepository extends JpaRepository<GES,String> {
+
+}
