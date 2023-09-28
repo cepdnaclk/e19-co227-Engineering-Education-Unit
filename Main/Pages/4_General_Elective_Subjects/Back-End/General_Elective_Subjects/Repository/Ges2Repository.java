69847af939e340b6b2a7ce5d70323bd6e63fb19d
Danujan <sivasundaralingamdanujan@gmail.com>
2023-09-28 17:56:39 +0530

    Ges2 Repository

---
+++ Main/Pages/4_General_Elective_Subjects/Back-End/General_Elective_Subjects/Repository/Ges2Repository.java
@@ -0,0 +1,9 @@
+package com.GES.General_Elective_Subjects.Repository;
+
+import com.GES.General_Elective_Subjects.Model.GES;
+import com.GES.General_Elective_Subjects.Model.Ges2;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Ges2Repository extends JpaRepository<Ges2,String> {
+
+}
