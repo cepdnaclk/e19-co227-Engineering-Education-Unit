f0fb23083e1fc2491ac709221b20111891716118
Danujan <sivasundaralingamdanujan@gmail.com>
2023-09-28 09:52:14 +0530

    Teal first component react

---
+++ Main/Pages/6_TEAL/Teal1.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Teal1.css'
+
+const Teal1 = () => {
+  return (
+    <div className = 'Main'>
+        <h2 className = 'Heading'>TEAL2.O Support and Development Centre</h2>
+        <p classname = "p1">The TEAL2.O project is implemented by a consortium of 11 universities from Sri Lanka, India, Thailand, Italy, Norway and Romania, joined by a private company from Bulgaria. The project seeks to improve access to Science and Technology higher education and reach underrepresented and disadvantaged groups. We believe this goal can be achieved by investing more effort into Open and Distance Learning. We are aware of the various obstacles faced by remote learning at low-resource institutions due to quality issues, lack of experience and technology, and lack of collaboration and internationalization. The project proposes a comprehensive technological solution - an open-access platform for technology-enhanced learning environments.</p>
+        
+        <br></br>
+        <p className = "p2">The platform will be maintained, exploited and disseminated by the TEAL2.O support and development centre under the Engineering Education Unit of the Faculty of Engineering of the University of Peradeniya.</p>
+    </div>
+  )
+}
+
+export default Teal1
