7c28165231c0d6e316c9ab90b0b67a14e45d8bcb
Danujan <sivasundaralingamdanujan@gmail.com>
2023-09-29 18:57:45 +0530

    Home User java Script files

---
+++ Main/Pages/1_Home/Front-End/HomeUser.js
@@ -0,0 +1,61 @@
+import React,{Component} from "react";
+import "./Home.css";
+import axios from 'axios';
+
+class HomeUser extends Component{
+    state = {
+        value: "Some text here",
+        isInEditMode: false,
+        code: "Home"
+    }
+
+    componentDidMount() {
+    axios.get("http://localhost:8080/gettext")
+        .then(response => {
+        const data = response.data;
+        const subjectWithCodeHello = data.find(item => item.code === "Home");
+    
+        if (subjectWithCodeHello) {
+            this.setState({
+            value: subjectWithCodeHello.subject
+            });
+        } else {
+            console.error("No data found with code 'hello'");
+        }
+        })
+        .catch(error => {
+        console.error("Error fetching paragraph:", error);
+        });
+    }
+
+    
+
+    renderDefaultView = () =>{
+        return <div > {/*onDoubleClick = {this.changeEditMode}
+    <button onClick = {this.changeEditMode}>Edit</button>*/}
+            <div className = "home_container">
+                <div className = "home-inner-container">
+                    <div className = "info">
+                    <textarea
+                        className = "info"
+                        value={this.state.value}
+                        // Adjust the number of columns as needed
+                        rows={25} // Adjust the number of rows as needed
+                        cols={100} // Adjust the number of columns as needed
+                        readOnly
+                        />
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+    }
+        
+    render(){
+    return this.state.isInEditMode ? this.renderEditView(): this.renderDefaultView()
+    
+    }
+}
+
+
+export default HomeUser;
