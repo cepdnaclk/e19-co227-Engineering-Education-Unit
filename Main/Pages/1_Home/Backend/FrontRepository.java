20cef9f8a433737412c311050171a471efbaef53
Danujan <sivasundaralingamdanujan@gmail.com>
2023-09-29 18:58:45 +0530

    Home page repository java file

---
+++ Main/Pages/1_Home/Backend/FrontRepository.java
@@ -0,0 +1,8 @@
+package com.GES.General_Elective_Subjects.Repository;
+
+import com.GES.General_Elective_Subjects.Model.Front;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FrontRepository extends JpaRepository<Front,String> {
+}
