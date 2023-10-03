83cf3211d3af5cfcc7af3d3415f143533735fcb1
Danujan <sivasundaralingamdanujan@gmail.com>
2023-10-03 23:08:21 +0530

    Menu data for the navbar

---
+++ Main/Components/Navbar/MenuData.js
@@ -0,0 +1,101 @@
+const menuData = [
+    {
+      "title": "Home",
+      "icon": "bi bi-house",
+      "path": "/"
+    },
+    {
+      "title": "Staff",
+      "icon": "bi bi-person",
+      "children": [
+        {
+          "title": "Present Staff",
+          "path": "/"
+        },
+        {
+          "title": "Past Staff",
+          "path": "/"
+        }
+      ]
+    },
+    {
+      "title": "General Elective Subjects",
+      "icon": "bi bi-book",
+      "path": "/"
+    },
+    {
+      "title": "TEAL 2.0 Support and Development Center",
+      "icon": "bi bi-file-easel",
+      "children": [
+        {
+          "title": "Support and Development Center",
+          "path": "/"
+        },
+        {
+          "title": "TEAL 2.0 Seminar Series",
+          "path": "/"
+        }
+      ]
+    },
+    {
+      "title": "Facilities",
+      "icon": "bi bi-hash",
+      "children": [
+        {
+          "title": "Seminar Room 1",
+          "path": "/"
+        },
+        {
+          "title": "Seminar Room 2",
+          "path": "/"
+        },
+        {
+          "title": "Seminar Room 3",
+          "path": "/"
+        },
+        {
+            "title": "A/V Studio",
+            "path": "/"
+          },
+          {
+            "title": "EOE Perera Theater",
+            "path": "/"
+          },
+          {
+            "title": "Conference Room",
+            "path": "/"
+          },
+          {
+            "title": "Lecture Rooms",
+            "path": "/"
+          },
+          {
+            "title": "Public Addressing System",
+            "path": "/"
+          },
+          {
+            "title": "Booking System",
+            "path": "/"
+          }
+      ]
+    },
+    {
+      "title": "Developments",
+      "icon": "bi bi-journal-bookmark",
+      "path": "/"
+    },
+    {
+      "title": "Help",
+      "icon": "bi bi-hand-thumbs-up-fill",
+      "children": [
+        {
+          "title": "Contact Us",
+          "path": "/"
+        }
+      ]
+    },
+    
+  ];
+  
+  export default menuData;
+  
