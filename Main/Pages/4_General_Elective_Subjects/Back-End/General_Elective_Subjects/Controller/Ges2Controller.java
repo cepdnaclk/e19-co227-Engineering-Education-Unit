ea35c56c9d7e1dcd7c097744c7835c9ee8da690b
Danujan <sivasundaralingamdanujan@gmail.com>
2023-10-09 16:53:06 +0530

    Updated with edit and delete functionalities

--- Main/Pages/4_General_Elective_Subjects/Back-End/General_Elective_Subjects/Controller/Ges2Controller.java
+++ Main/Pages/4_General_Elective_Subjects/Back-End/General_Elective_Subjects/Controller/Ges2Controller.java
@@ -1,5 +1,6 @@
 package com.GES.General_Elective_Subjects.Controller;
 
+import com.GES.General_Elective_Subjects.Model.GES;
 import com.GES.General_Elective_Subjects.Model.Ges2;
 
 import com.GES.General_Elective_Subjects.Repository.Ges2Repository;
@@ -23,5 +24,25 @@ public class Ges2Controller {
     @GetMapping("/getcourses2")
     List<Ges2> getAllCourses(){
         return ges2Repository.findAll();
+    }
+
+    @PutMapping("/user2/{code}")
+    Ges2 editGes(@PathVariable String code, @RequestBody Ges2 newGes){
+        Ges2 ges2 = ges2Repository.findById(code).get();
+        ges2.setSubject(newGes.getSubject());
+        ges2.setCredits(newGes.getCredits());
+        ges2.setCategory(newGes.getCategory());
+        ges2.setSemester_5_7(newGes.getSemester_5_7());
+        ges2.setSemester_6_8(newGes.getSemester_6_8());
+        ges2.setcO_Short_Semester(newGes.getcO_Short_Semester());
+        ges2.setCoordinating_Dept(newGes.getCoordinating_Dept());
+        ges2.setCoordinator(newGes.getCoordinator());
+        ges2.setLecturer(newGes.getLecturer());
+        return ges2Repository.save(ges2);
+    }
+
+    @DeleteMapping("/user2/{code}")
+    void deleteGes(@PathVariable String code){
+        ges2Repository.deleteById(code);
     }
 }
